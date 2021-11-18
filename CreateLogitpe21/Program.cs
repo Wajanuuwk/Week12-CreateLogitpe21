93458dfec30779ac469b2d0271e8c79e6227453f
Opilane TTHK <opilane@tthk.ee>
2021-11-18 10:19:03 +0200

    CreateLogitpe21

---
+++ CreateLogitpe21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CreateLogitpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newDirectory = "logitpe21";
+            CreateMyDirectory(newDirectory);
+        }
+
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userDirectory = "logitpe21";
+            string directoryFullPath = $@"{rootDirectory}\{userDirectory}";
+
+            bool directoryExists = Directory.Exists(directoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} already exists in {rootDirectory}.");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryFullPath);
+                Console.WriteLine($"{userDirectory} has been created in {rootDirectory}.");
+            }
+        }
+
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = $@"C: \Users\opilane\samples";
+            string directoryFullPath = $@"{rootDirectory}\{newDirectory}";
+        }
+    }
+}
---
+++ Quotes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Quotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetRandomQuote());
+        }
+
+        public static string GetRandomQuote()
+        {
+            string filePath = @"C:\Users\opilane\samples\49 Wise Quotes That Will Inspire You to Succeed in Life.txt";
+            string[] wiseQuotes = File.ReadAllLines(filePath);
+            Console.WriteLine(wiseQuotes[0]);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, wiseQuotes.Length);
+
+            return wiseQuotes[randomIndex];
+        }
+    }
+}
